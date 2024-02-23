b91b0f8f0843826cedebe884a67f61fce166e076
sayedaffan1 <159750144+sayedaffan1@users.noreply.github.com>
2024-02-23 11:02:07 +0530

    Add files via upload

---
+++ cradit score.py
@@ -0,0 +1,12 @@
+score = int(input("Enter your credit score: "))
+
+if score >= 750:
+  print("Excellent!")
+elif score >= 700:
+  print("Good!")
+elif score >= 650:
+  print("Fair!")
+elif score >= 600:
+  print("Poor!")
+else:
+  print("Bad!")
